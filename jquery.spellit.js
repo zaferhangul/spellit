5bd252425fd327b31352486ac580f5354c1eba5d
Zafer <zfrhngl@gmail.com>
2013-07-30 13:54:29 +0300

    Create jquery.spellit.js

---
+++ jquery.spellit.js
@@ -0,0 +1,67 @@
+/*----------------------------------------------------
+  Spellit v1.0 - 29/07/2013
+	jQuery nonsensical spelling plugin
+	Zafer Hangul - http://www.zaferhangul.com/spellit
+-----------------------------------------------------*/
+(function($){
+ 	$.fn.extend({ 
+ 		spellit: function(options) {
+
+			var defaults = {
+				timeout:40,
+				effect: '', // scaleMin - scaleBig - starWars - rotate
+				//loop: 0,
+				element: 'span' 
+
+			};
+			
+			var options = $.extend(defaults, options);
+			
+    		return this.each(function() {
+			
+			var obj = $(this);
+			var str = obj.text().trim();
+			//console.log(str);
+			var firstLength = str.length;
+			resultArray=[];
+			var opener = "<"+defaults.element+" class="+defaults.effect+">";
+			var closer = "</"+defaults.element+">";
+			
+				for(var i=0;i<firstLength;i++){
+					resultArray.push(opener);
+					if(str[i]==" "){ 
+						resultArray.push(str[i].replace(" ","&nbsp;"));
+					} else {
+						resultArray.push(str[i]);
+						}
+					resultArray.push(closer);
+				}
+				
+				var result= resultArray.join("");
+				obj.html(result);
+				if(defaults.effect==''){
+					obj.find(defaults.element).hide();
+				}
+				
+
+				obj.find(defaults.element).each(function(){
+					var i = $(this).index();
+					var ms = i*defaults.timeout;
+					var $this = $(this);
+					setTimeout(function(){
+						if(defaults.effect==''){
+							$this.show();
+						} else {
+						$this.addClass("play");
+						}
+						//$this.addClass(effectClass);
+						//console.log(ms);
+					},ms);
+					
+					
+				});
+			
+    		});
+    	}
+	});
+})(jQuery);
